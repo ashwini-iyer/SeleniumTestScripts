904060ed3a88f158aa2a491db5d77ea7a6954697
Ashwini <ashwini.itc@gmail.com>
2021-08-21 11:10:51 -0700

    Add files via upload
    GO to website https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt and click on try it and then check if the name you entered in box was visible in text

--- DemoTestApp.java
+++ DemoTestApp.java
@@ -2,10 +2,12 @@ package demo;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DemoTestApp {
@@ -14,13 +16,26 @@ public class DemoTestApp {
 
 		System.setProperty("webdriver.gecko.driver", "c:\\driver\\geckodriver.exe");
 		WebDriver d = new FirefoxDriver();
+		//WebDriver d = new EdgeDriver();
 		d.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		d.get("https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt"
 				);
 		d.switchTo().frame("iframeResult");
 		WebElement we = d.findElement(By.xpath("//button[text()='Try it']"));
-		System.out.println(we);
+		
 		we.click();
+		Alert we1 = d.switchTo().alert();
+		we1.sendKeys("Ashwini Iyer");
+		we1.accept();
+		//d.switchTo().parentFrame();
+		
+		//d.switchTo().frame("iframeResult");
+		System.out.println(d.findElement(By.xpath("/html/body/p")));
+		String sentence = d.findElement(By.xpath("/html/body/p")).getText();
+		if(sentence.contains("Ashwini Iyer")) {
+			System.out.println("The name was found !");
+		}
+		
 		
 	}
 
